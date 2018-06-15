1bdca9eb7c26359c85d365b4e4cd7cbdb535bc95
LYthe <2270779418@qq.com>
2018-06-15 14:57:51 +0800

    select sort

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+"""
+选择排序
+最优时间复杂度：O（n*n）
+最坏时间复杂度：O（n*n）
+稳定性：不稳定（考虑升序每次选择最大的情况）
+"""
+
+
+def select_sort(my_list):
+    for i in range(len(my_list)):
+        smallest = i
+        for j in range(i+1, len(my_list)):
+            if my_list[smallest] > my_list[j]:
+                smallest = j
+        my_list[smallest], my_list[i] = my_list[i], my_list[smallest]
+    return my_list
+
+
+arr = [10, 5, 84, 4, 3]
+print(select_sort(arr))
